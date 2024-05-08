46708b78aeddad8ce9b141a7ee39d6df26432dc6
zainebl001 <166256158+zainebl001@users.noreply.github.com>
2024-05-08 03:42:43 -0700

    Update script.js

--- m05/script.js
+++ m05/script.js
@@ -1,20 +1,19 @@
- var number = prompt("Enter a number from 0 to 10:");
+var number = prompt("Enter a number from 0 to 10:");
 
-      var msg = "<h2>Multiplication Table</h2>";
-      msg += "<table>";
+var msg = "<h2>Multiplication Table</h2>";
+msg += "<table>";
 
-      for (var i = 1; i <= 10; i++) {
-        msg +=
-          "<tr><td>" +
-          i +
-          " x " +
-          number +
-          " = </td><td>" +
-          number * i +
-          "</td></tr>";
-      }
+for (var i = 1; i <= 10; i++) {
+  msg +=
+    "<tr><td>" +
+    i +
+    " x " +
+    number +
+    " = </td><td>" +
+    number * i +
+    "</td></tr>";
+}
 
-      msg += "</table>";
+msg += "</table>";
 
-      document.getElementById("blackboard").innerHTML = msg;
-    </script>
+document.getElementById("blackboard").innerHTML = msg;
