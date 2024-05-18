f33916a3ff3b8e644bce8d4b27269b1eba2f4cc5
zainebl001 <166256158+zainebl001@users.noreply.github.com>
2024-05-18 13:06:05 -0700

    Update script.js

--- m07/js/script.js
+++ m07/js/script.js
@@ -1,1 +1,30 @@
+function Hotel(name, rooms, booked) {
+  this.name = name;
+  this.rooms = rooms;
+  this.booked = booked;
+  this.checkAvailability = function() {
+    return this.rooms - this.booked;
+  };
+}
 
+
+// Create three hotel objects
+var quayHotel = new Hotel('Quay', 40, 25);
+var parkHotel = new Hotel('Park', 120, 77);
+var sunsetHotel = new Hotel('Sunset', 86, 10);
+
+// Update the HTML for the page
+var details1 = quayHotel.name + ' rooms: ';
+    details1 += quayHotel.checkAvailability();
+var elHotel1 = document.getElementById('hotel1');
+elHotel1.textContent = details1;
+
+var details2 = parkHotel.name + ' rooms: ';
+    details2 += parkHotel.checkAvailability();
+var elHotel2 = document.getElementById('hotel2');
+elHotel2.textContent = details2;
+
+var details3 = sunsetHotel.name + ' rooms: ';
+    details3 += sunsetHotel.checkAvailability();
+var elHotel3 = document.getElementById('hotel3');
+elHotel3.textContent = details3;
