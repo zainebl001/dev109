123bf848680b580d5a7ffb64dda5070b5b1421e3
zainebl001 <166256158+zainebl001@users.noreply.github.com>
2024-05-26 03:01:13 -0700

    Update script.js

--- m08/js/script.js
+++ m08/js/script.js
@@ -1,15 +1,18 @@
-// Create a new element and store it in a variable.
-var newEl = document.createElement('li');
 
-// Create a text node and store it in a variable.
-var newText = document.createTextNode('quinoa');
-
-// Attach the new text node to the new element.
-newEl.appendChild(newText);
-
-// Find the position where the new element should be added.
-var position = document.getElementsByTagName('ul')[0];
-
-// Insert the new element into its position.
-position.appendChild(newEl);
-
+function addItem() {
+  var input = document.getElementById('newItem').value;
+  if (input.trim() !== "") {
+    
+    var newEl = document.createElement('li');
+    
+    var newText = document.createTextNode(input);
+    
+    newEl.appendChild(newText);
+    
+    var position = document.getElementById('todo');
+    
+    position.appendChild(newEl);
+    
+    document.getElementById('newItem').value = "";
+  }
+}
