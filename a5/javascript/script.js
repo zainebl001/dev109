1a87c844dff167212b96d4dadc21aba6f1601b56
zainebl001 <166256158+zainebl001@users.noreply.github.com>
2024-06-02 04:18:12 -0700

    Update script.js

--- a5/javascript/script.js
+++ a5/javascript/script.js
@@ -1,57 +1,66 @@
-  var myImages = [
-    "https://uhdwallpapers.org/uploads/converted/19/04/03/spring-bee-blossom-flower-picture-1920x1080_479654-mm-90.webp",
-    "https://h2.gifposter.com/bingImages/HoneycombBee_1920x1080.jpg",
-    "https://wallpaperswide.com/download/bee_20-wallpaper-1920x1080.jpg",
-    "https://c.wallhere.com/photos/b8/e5/1920x1080_px_bee_Bees_flower_flowers_insect-1657467.jpg!d",
-    "https://wallpaperfx.com/view_image/swarm-of-bees-1920x1080-wallpaper-16962.jpg"
-  ];
-  var captionImages = ["Pollinating", "Honeycomb", "Landing", "Close-Up", "Swarm"];
-  var index = 0;
+var myImages = [
+  "https://uhdwallpapers.org/uploads/converted/19/04/03/spring-bee-blossom-flower-picture-1920x1080_479654-mm-90.webp",
+  "https://h2.gifposter.com/bingImages/HoneycombBee_1920x1080.jpg",
+  "https://wallpaperswide.com/download/bee_20-wallpaper-1920x1080.jpg",
+  "https://c.wallhere.com/photos/b8/e5/1920x1080_px_bee_Bees_flower_flowers_insect-1657467.jpg!d",
+  "https://wallpaperfx.com/view_image/swarm-of-bees-1920x1080-wallpaper-16962.jpg"
+];
+var captionImages = [
+  "This image depicts a bee pollinating a mirabelle plum (Prunus domestica subsp. syriaca) tree.",
+  "This image depicts a bee interacting with honeycomb strengthening the hive.",
+  "This image depicts a bee landing on a latice structure.",
+  "This image depicts a close up intircate photograph of a bee.",
+  "This image depicts a swarm of over 100 bees in their hive."
+];
+var index = 0;
 
-  function updateImage() {
-    document.getElementById("slideshow").src = myImages[index];
-    document.getElementById("slideshow").alt = captionImages[index];
-    document.getElementById("caption").textContent = captionImages[index];
+function updateImage() {
+  document.querySelector(".slideshow-image").src = myImages[index];
+  document.querySelector(".slideshow-image").alt = captionImages[index];
+  document.querySelector(".caption").textContent = captionImages[index];
+}
+
+function next() {
+  if (index === myImages.length - 1) {
+    index = 0;
+  } else {
+    index++;
   }
+  updateImage();
+  restartInterval();
+}
 
-  function next() {
-    if (myImages.length == index + 1)
-      index = 0;
-    else
-      index++;
-    updateImage();
-    restartInterval();
+function back() {
+  if (index === 0) {
+    index = myImages.length - 1;
+  } else {
+    index--;
   }
+  updateImage();
+  restartInterval();
+}
 
-  function back() {
-    if (index === 0)
-      index = myImages.length - 1;
-    else
-      index--;
-    updateImage();
-    restartInterval();
+var nextButton = document.getElementById("next");
+var previousButton = document.getElementById("previous");
+
+previousButton.addEventListener("click", back, false);
+nextButton.addEventListener("click", next, false);
+
+function autoSlide() {
+  if (document.getElementById("auto").checked) {
+    next();
   }
+}
 
-  var nextButton = document.getElementById("next");
-  var previousButton = document.getElementById("previous");
+var intervalId;
 
-  previousButton.addEventListener("click", back, false);
-  nextButton.addEventListener("click", next, false);
+function startInterval() {
+  intervalId = setInterval(autoSlide, 2000);
+}
 
-  function autoSlide() {
-    if (document.getElementById("auto").checked)
-      next();
-  }
-
-  var intervalId;
-
-  function startInterval() {
-    intervalId = setInterval(autoSlide, 2000);
-  }
-
-  function restartInterval() {
-    clearInterval(intervalId);
-    startInterval();
-  }
-
+function restartInterval() {
+  clearInterval(intervalId);
   startInterval();
+}
+
+startInterval();
